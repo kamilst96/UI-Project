567b8f52c9506a7dcc8dcdc6241c019c70c1d6ea
Kamil Stępień <kamilst96@gmail.com>
2018-03-16 22:45:48 +0100

    Add anchor scroll offset to compensate for sticky navbar.

--- js/anchor.js
+++ js/anchor.js
@@ -6,11 +6,13 @@
 $(document).ready(function () {
   /* Set whole html body as the field for animation */
   var $root = $('html, body');
+  /* Make up for sticky navbar */
+  var scroll_offset = 50;
 
   /* Listen for click event */
   $('nav a, footer a').click(function () {
     $root.animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
+      scrollTop: $($(this).attr('href')).offset().top - scroll_offset
     }, 300);
   });
 });
