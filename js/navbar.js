7fe7b3e2eef5f28baf20d4dd813145e7a0b15805
Kamil Stępień <kamilst96@gmail.com>
2018-03-18 17:30:18 +0100

    Again optimize navbar code.

--- js/navbar.js
+++ js/navbar.js
@@ -4,23 +4,19 @@
 
 $(function () {
   /* Initialize variables */
-  var $navitems = $('nav a:not(.navtoggle)');
+  var $nav = $('nav a');
   var $navtoggle = $('nav a.navtoggle');
 
   /* Listen for click event */
   $navtoggle.click(function () {
 
     /* Toggle menu visibility */
-    $navitems.each(function () {
+    $nav.each(function () {
       $(this).toggleClass('responsive');
     });
 
-    $navtoggle.each(function () {
-      $(this).toggleClass('responsive');
-    })
-
     /* Change navtoggle mode */
-    var symbol = $navitems.first().hasClass('responsive') ? '&#9652;' : '&#9662;';
+    var symbol = $nav.first().hasClass('responsive') ? '&#9652;' : '&#9662;';
     $navtoggle.html(symbol + ' Menu ' + symbol);
   });
 });
